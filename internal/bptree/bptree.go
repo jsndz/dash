b56662f17ec7a2500aec9bb6b3e4a1b9637ec383
jsndz <jaisondz9360@gmail.com>
2026-06-22 11:29:18 +0530

    insert to leaf

--- internal/bptree/bptree.go
+++ internal/bptree/bptree.go
@@ -68,6 +68,35 @@ func (tree *Bptree) SearchLeafNode(command string, node *Node) *Command {
 	return nil
 }
 
+func (tree *Bptree) InsertInRecursion() {
+
+}
+
+// from my idea of insert
+// first lets find the node through searching
+// if already exist increase the frequency
+// if does not exist take its index and parent index
+// and insert
+// insert has 3 conditions
+// insert and max size wont be crossed
+// insert and max size will cross
+// where you have to split the node and
+
+func FindLeaf(node *Node, cmd string) *Node {
+	if node.IsLeaf {
+		return node
+	}
+	index := sort.Search(len(node.Keys), func(i int) bool {
+		return node.Keys[i] > cmd
+	})
+	// from this moment there are two options
+	// if the value we are searching is same as key
+	// or the value we are searching is pointing to some children
+	if len(node.Keys) > 1 {
+	}
+	return FindLeaf(node.Children[index], cmd)
+}
+
 func (tree *Bptree) Insert(cmdText string) {
 	command := &Command{
 		Text:      cmdText,
@@ -76,16 +105,37 @@ func (tree *Bptree) Insert(cmdText string) {
 	}
 
 	node := tree.Root
+	// you have root search for >= in the node keys
+	leafNode := FindLeaf(node, cmdText)
+	InsertIntoLeaf(leafNode, command)
+}
+
+func InsertIncreaseFrequency(command *Command) {
+	command.Frequency++
+	command.LastUsed = time.Now()
+}
+
+func InsertIntoLeaf(node *Node, command *Command) {
 	if len(node.Values) == 0 {
 		node.Values = append(node.Values, command)
 		return
 	}
 
 	i := sort.Search(len(node.Values), func(i int) bool {
-		return node.Values[i].Text > cmdText
+		return node.Values[i].Text >= command.Text
 	})
+	if i < len(node.Keys) && node.Keys[i] == command.Text {
+		InsertIncreaseFrequency(command)
+		return
+	}
+
+	// add keys also since they are direct pointer to values
+	node.Keys = append(node.Keys, "")
+	copy(node.Keys[i+1:], node.Keys[i:])
+	node.Keys[i] = command.Text
 
 	node.Values = append(node.Values, nil)
 	copy(node.Values[i+1:], node.Values[i:])
 	node.Values[i] = command
+
 }
--- internal/bptree/bptree_test.go
+++ internal/bptree/bptree_test.go
@@ -8,9 +8,13 @@ func TestRootAsLeaf(t *testing.T) {
 	t.Run("check if basic insert is working for 1 element", func(t *testing.T) {
 		tree := NewTree()
 
-		tree.Insert("ls")
-
-		cmd := tree.Search("ls", tree.Root)
+		node := &Node{
+			IsLeaf: true,
+			Keys:   []string{"ls"},
+			Values: []*Command{{Text: "ls"}},
+		}
+		tree.Root = node
+		cmd := tree.SearchLeafNode("ls", tree.Root)
 		expected := "ls"
 		if cmd.Text != expected {
 			t.Errorf("didn't get expected form search for %s got %s", expected, cmd.Text)
@@ -19,11 +23,13 @@ func TestRootAsLeaf(t *testing.T) {
 	t.Run("check if basic insert is working for more element", func(t *testing.T) {
 		tree := NewTree()
 
-		tree.Insert("ls")
-		tree.Insert("echo")
-		tree.Insert("docker ps")
-
-		cmd := tree.Search("ls", tree.Root)
+		node := &Node{
+			IsLeaf: true,
+			Keys:   []string{"ls"},
+			Values: []*Command{{Text: "ls"}, {Text: "la"}, {Text: "cd"}},
+		}
+		tree.Root = node
+		cmd := tree.SearchLeafNode("ls", tree.Root)
 		expected := "ls"
 		if cmd.Text != expected {
 			t.Errorf("didn't get expected form search for %s got %s", expected, cmd.Text)
@@ -41,6 +47,59 @@ func TestRootAsLeaf(t *testing.T) {
 	})
 }
 
-// func TestSearchInternalNode(t *testing.T) {
-// 	t.Run("")
-// }
+func TestSearchInternalNode(t *testing.T) {
+	t.Run("search with internal nodes", func(t *testing.T) {
+
+		leaf1 := &Node{
+			IsLeaf: true,
+			Keys:   []string{"apple", "banana", "cat"},
+			Values: []*Command{
+				{Text: "apple"},
+				{Text: "banana"},
+				{Text: "cat"},
+			},
+		}
+
+		leaf2 := &Node{
+			IsLeaf: true,
+			Keys:   []string{"dog", "elephant", "fish"},
+			Values: []*Command{
+				{Text: "dog"},
+				{Text: "elephant"},
+				{Text: "fish"},
+			},
+		}
+
+		leaf3 := &Node{
+			IsLeaf: true,
+			Keys:   []string{"goat", "horse", "iguana"},
+			Values: []*Command{
+				{Text: "goat"},
+				{Text: "horse"},
+				{Text: "iguana"},
+			},
+		}
+
+		leaf1.Next = leaf2
+		leaf2.Next = leaf3
+
+		root := &Node{
+			IsLeaf: false,
+			Keys:   []string{"dog", "goat"},
+			Children: []*Node{
+				leaf1, // < dog
+				leaf2, // >= dog && < goat
+				leaf3, // >= goat
+			},
+		}
+
+		tree := &Bptree{
+			Root: root,
+		}
+		cmd := tree.Search("goat", tree.Root)
+		expected := "goat"
+		if cmd.Text != expected {
+			t.Errorf("didn't get expected form search for %s got %s", expected, cmd.Text)
+		}
+	})
+}
