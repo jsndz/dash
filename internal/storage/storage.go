6e94e5c8bc263523825a188c68409b791773dfae
jsndz <jaisondz9360@gmail.com>
2026-06-30 13:08:46 +0530

    tree for serialisation

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,7 +2,6 @@ package storage
 
 import (
 	"dash/internal/tree"
-	"time"
 )
 
 type Disktree struct {
@@ -12,18 +11,12 @@ type Disktree struct {
 	Nodes   []*DiskNode
 }
 
-type Command struct {
-	Text      string
-	LastUsed  time.Time
-	Frequency int
-}
-
 type DiskNode struct {
 	ID         uint64
 	IsLeaf     bool
-	Keys       []uint64
+	Keys       []string
 	ChildrenID []uint64
-	Values     []*Command
+	Values     []tree.Command
 	ParentID   uint64
 	NextID     uint64
 	PrevID     uint64
@@ -38,9 +31,53 @@ func NewDisktree(rootID uint64, maxSize int, minSize int) *Disktree {
 	}
 }
 
-func Generate(tree *tree.Bptree) *Disktree {
+func Generate(t *tree.Bptree) *Disktree {
 	rootId := 1
 	disktree := NewDisktree(uint64(rootId), 128, 64)
-
+	mapper := make(map[*tree.Node]uint64)
+	id := uint64(1)
+	SetupMap(mapper, t.Root, &id)
+	traverse(t.Root, disktree, mapper)
 	return disktree
+}
+
+func SetupMap(treemap map[*tree.Node]uint64, node *tree.Node, id *uint64) {
+	treemap[node] = *id
+	*id++
+
+	for _, node := range node.Children {
+		SetupMap(treemap, node, id)
+	}
+
+}
+
+func traverse(node *tree.Node, disktree *Disktree, mapper map[*tree.Node]uint64) {
+	var children []uint64
+	var vals []tree.Command
+
+	if node.IsLeaf {
+		for _, cmd := range node.Values {
+			vals = append(vals, *cmd)
+		}
+	} else {
+		for _, ch := range node.Children {
+			children = append(children, mapper[ch])
+		}
+	}
+	treeNode := &DiskNode{
+		ID:         mapper[node],
+		IsLeaf:     node.IsLeaf,
+		Keys:       node.Keys,
+		ChildrenID: children,
+		Values:     vals,
+		ParentID:   mapper[node.Parent],
+		PrevID:     mapper[node.Prev],
+		NextID:     mapper[node.Next],
+	}
+	if !node.IsLeaf {
+		for _, ch := range node.Children {
+			traverse(ch, disktree, mapper)
+		}
+	}
+	disktree.Nodes = append(disktree.Nodes, treeNode)
 }
